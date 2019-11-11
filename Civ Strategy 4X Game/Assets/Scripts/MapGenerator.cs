64d35b8b4da013f0f4e4d6195fddd28544411b9a
lseper <lseper2@huskers.unl.edu>
2019-11-10 23:13:43 -0600

    Fixed render issues
    added order to the tilemaps so they rendered on top of one another

--- Civ Strategy 4X Game/Assets/Scripts/MapGenerator.cs
+++ Civ Strategy 4X Game/Assets/Scripts/MapGenerator.cs
@@ -9,7 +9,7 @@ public class MapGenerator : MonoBehaviour {
 	public Tilemap groundMap;
 	public Tilemap terrainMap;
 
-	public Tile[] landTiles = new Tile[20];
+	public Tile[] landTiles;
 
     // Start is called before the first frame update
     void Start () {
@@ -18,19 +18,21 @@ public class MapGenerator : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-        
+
     }
 
     //Randomly generate a new map of the given dimensions sizeX and sizeY
     void GenerateMap (int sizeX, int sizeY) {
     	for (int x = 0; x < sizeX; x++) {
         	for (int y = 0; y < sizeY; y++) {
+                // put the ground layer on the bottom layer (z = 0)
         		groundMap.SetTile(new Vector3Int(x, y, 0), landTiles[0]);
 
-        		int nextTile = Random.Range(0, 3);
+        		int nextTile = Random.Range(0, landTiles.Length);
 
+                // put the terrain on top(z = 1)
         		if (nextTile > 0) {
-        			terrainMap.SetTile(new Vector3Int(x, y, 0), landTiles[nextTile]);
+        			terrainMap.SetTile(new Vector3Int(x, y, 1), landTiles[nextTile]);
         		}
         	}
         }
